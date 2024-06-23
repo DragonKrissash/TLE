48965fd8679d77027fc6fba2155780e3a06e867b
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-06-23 19:59:32 +0530

    24-5-2024

---
+++ Level 1/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define vi vector<int>
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+        string s;cin>>s;
+    if(s.length()<10)
+    cout<<s<<endl;
+    else cout<<s[0]<<s.length()-2<<s[s.length()-1]<<endl;
+    }
+}
