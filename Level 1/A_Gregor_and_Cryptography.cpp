1bbeef492859620f182d898f2cb18dfd5a48a43e
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-06-30 11:30:04 +0530

    30-6-2024

---
+++ Level 1/A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define vi vector<int>
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+        ll p;cin>>p;
+        cout<<2<<" "<<p-1<<endl;
+    }
+}
