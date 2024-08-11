8c1218eaa2f473f5111c13d280e3124793c76075
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-08-11 12:00:21 +0530

    11-8-2024[A

---
+++ Practice/800/B_Symmetric_Encoding.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+void solve(){
+    int n;cin>>n;
+    string s;cin>>s;
+    set<char>st;
+    for(int a=0;a<n;a++){
+        st.insert(s[a]);
+    }
+    string enc;
+    for(auto c:st){
+        enc+=c;
+    }
+    string revenc=enc;
+    reverse(all(revenc));
+    map<char,char>mp;
+    for(int a=0;a<revenc.size();a++){
+        mp[revenc[a]]=enc[a];
+    }
+    for(int a=0;a<n;a++){
+        cout<<mp[s[a]];
+    }
+    cout<<nl;
+}
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+
+
+// template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+// template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+// struct starter{
+//    high_resolution_clock::time_point t1, t2;
+
+//    starter() {
+//        ios::sync_with_stdio(0); cin.tie(0);
+//        t1 = high_resolution_clock::now();
+//    }
+//    ~starter() {
+//        t2 = high_resolution_clock::now();
+//        duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//        cerr <<  time_span.count();
+//        }
+// } starter_;
