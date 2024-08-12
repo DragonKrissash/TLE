0ec5c37144e1cb5ffeab202a6701c82ba8d8e33b
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-08-12 19:21:35 +0530

    12-8-2024

---
+++ Practice/800/B_Prefiquence.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+void solve(){
+    int n,m;cin>>n>>m;
+    string s,t;cin>>s>>t;
+    int i=0,j=0;
+    int cnt=0;
+    while(i<n and j<m){
+        if(s[i]==t[j]){
+            cnt++;
+            j++;
+            i++;
+        }
+        else{
+            j++;
+        }
+    }
+    cout<<cnt<<nl;
+}
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
