8ef622a8cbaf2935fec0f3917d755b7773aff063
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-08-16 11:58:25 +0530

    16-8-2024

---
+++ Practice/900/A_Dual_Trigger.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+void solve(){
+    int n;cin>>n;
+    string s;cin>>s;
+    int cnt=0;
+    int mi=INT_MAX;
+    int mx=INT_MIN;
+    for(int a=0;a<n;a++){
+        if(s[a]=='1'){
+            cnt++;
+            mi=min(mi,a);
+            mx=max(mx,a);
+        }
+    }
+    if(cnt&1){
+        no;
+    }
+    else if(cnt==2 and mx-mi==1){
+        no;
+    }
+    else{
+        yes;
+    }
+}
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
