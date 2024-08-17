fa18cc3bcd390e5ea9bf6c08e7518cc6a9537835
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-08-17 18:44:59 +0530

    17-8-2024

---
+++ Practice/1100/B_Maximum_Sum.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+const int MOD=1e9+7;
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+const int P = 1e9 + 7;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int S = 0, sum = 0;
+    int cur = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        cur += a[i];
+        cur = max(cur, 0LL);
+        S = max(S, cur);
+    }
+    sum = (sum % P + P) % P;
+    S = S % P;
+    int t = 1;
+    for (int i = 0; i < k; i++) {
+        t = t * 2 % P;
+    }
+    int ans = (sum + S * t - S + P) % P;
+    cout << ans << '\n';
+}
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
