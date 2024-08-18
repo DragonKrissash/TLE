e985de28ec99697f49bfc0c4d7f23ac5154fb0f0
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-08-18 19:40:16 +0530

    18-8-2024

---
+++ Practice/800/A_Setting_up_Camp.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+void solve(){
+    int a,b,c,d=0;cin>>a>>b>>c;
+    if(b%3!=0)d=3-b%3;
+    if(d>c)cout<<-1<<nl;
+    else
+    cout<<a+(b+d)/3+(c-d+2)/3<<nl;
+}
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
