d7158df4d6efd1fa5ff1a90211d53bb110781c4e
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-08-28 21:14:28 +0530

    28-8-2024

---
+++ Practice/1500/D_2_Encrypting_Messages.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+int add(int a,int b,int mod){
+    int ans=(a%mod + b%mod)%mod;
+    if(ans<0){
+        ans+=mod;
+    }
+    return ans;
+}
+
+int sub(int a,int b,int mod){
+    int ans=(a%mod - b%mod)%mod;
+    if(ans<0)ans+=mod;
+    return ans;
+}
+
+void solve(){
+    int n,m,c;cin>>n>>m>>c;
+    vi arr(n);
+    input(arr);
+    vi b(m);
+    vi t(n,0);
+    for(int a=0;a<m;a++){
+        cin>>b[a];
+        t[a]=add(t[a],b[a],c);
+        if(n-m+a+1<n)
+        t[n-m+a+1]=sub(t[n-m+a+1],b[a],c);
+    }
+    int sum=0;
+    repi(0,n-1,1){
+        sum+=t[i]%c;
+        sum=sum%c;
+        t[i]=sum;
+        cout<<(arr[i]%c+t[i]%c)%c<<sp;
+    }
+    cout<<nl;
+    
+}
+
+signed main(){
+
+    fastio
+    int tc=1;
+    while(tc--){
+    solve();
+    }
+}
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
