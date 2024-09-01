6865ae2cc23ce81dcbe3884daef3892cd8e90d41
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-09-01 17:16:25 +0530

    1-9-2024

---
+++ Practice/1200/B_Stone_Age_Problem.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+
+int add(int a,int b,int mod){
+   int ans=(a%mod + b%mod)%mod;
+   if(ans<0){
+      ans+=mod;
+   }
+   return ans;
+}
+
+int sub(int a,int b,int mod){
+   int ans=(a%mod - b%mod)%mod;
+   if(ans<0)ans+=mod;
+   return ans;
+}
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc=1;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,q;cin>>n>>q;
+   map<int,int>mp;
+   int sum=0;
+   repi(1,n,1){
+    int x;cin>>x;
+    mp[i]=x;sum+=x;
+   }
+   int setter=-1;
+    repi(1,q,1){
+        int x;cin>>x;
+        if(x==1){
+            int y,z;cin>>y>>z;
+            if(mp.find(y)==mp.end()){
+                sum-=setter;
+            }
+            else
+            sum-=mp[y];
+            mp[y]=z;
+            sum+=mp[y];
+            cout<<sum<<nl;
+        }
+        else {
+            int y;cin>>y;
+            setter=y;
+            sum=y*n;
+            mp.clear();
+            cout<<sum<<nl;
+        }
+    }
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
