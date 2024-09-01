6971c9306a1a687dc2d9683bca4bc69bef41bd88
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-09-01 17:16:43 +0530

    1-9-2024

---
+++ Practice/900/A_AvtoBus.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+
+int add(int a,int b,int mod){
+   int ans=(a%mod + b%mod)%mod;
+   if(ans<0){
+      ans+=mod;
+   }
+   return ans;
+}
+
+int sub(int a,int b,int mod){
+   int ans=(a%mod - b%mod)%mod;
+   if(ans<0)ans+=mod;
+   return ans;
+}
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    if(n&1)cout<<-1<<nl;
+    else if(n%4==0 and n%6==0)cout<<n/6<<sp<<n/4<<nl;
+    else{
+        n=n/2;
+        if(n==1)cout<<-1<<nl;
+        else cout<<((n%3==0)? n/3:n/3+1)<<sp<<n/2<<nl; 
+    }
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
