2ca1789a6852619d56d608fa6e3c71fa79258b47
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-09-03 19:29:55 +0530

    3-9-2024

---
+++ Practice/1200/C_Make_Equal_With_Mod.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+
+int add(int a,int b,int mod){
+   int ans=(a%mod + b%mod)%mod;
+   if(ans<0){
+      ans+=mod;
+   }
+   return ans;
+}
+
+int sub(int a,int b,int mod){
+   int ans=(a%mod - b%mod)%mod;
+   if(ans<0)ans+=mod;
+   return ans;
+}
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n);
+    input(v);
+    bool one=0,two=0,zero=0;
+    set<int>st;
+    for(auto i:v){
+        if(i==1)one=1;
+        else if(i==2)two=1;
+        else if(i==0)zero=1;
+        st.insert(i);
+    }
+    if(one && (two||zero)){
+        no;
+        return;
+    }
+    else if(one){
+        for(auto i:st){
+            if(st.find(i-1)!=st.end()){
+                no;
+                return;
+            }
+        }
+    }
+    yes;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
