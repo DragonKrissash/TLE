e03e0abfe1e0f01d2a95a48cc48cf57d26df8052
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-09-12 18:03:54 +0530

    12-9-2024

---
+++ Practice/1400/B_Jumping_on_Walls.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+
+int add(int a,int b,int mod){
+   int ans=(a%mod + b%mod)%mod;
+   if(ans<0){
+      ans+=mod;
+   }
+   return ans;
+}
+
+int sub(int a,int b,int mod){
+   int ans=(a%mod - b%mod)%mod;
+   if(ans<0)ans+=mod;
+   return ans;
+}
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc=1;
+    while(tc--){
+    solve();
+    }
+}
+int n,k;
+int len;
+vector<string>wall(2);
+bool dfs(int i,int j,int p){
+    if(j>=n)return 1;
+    if(wall[i][j]!='-'||p>j)return 0;
+    wall[i][j]='X';
+    return dfs(1-i,j+k,p+1)||dfs(i,j+1,p+1)||dfs(i,j-1,p+1);
+}
+void solve(){
+    cin>>n>>k>>wall[0]>>wall[1];
+    if(dfs(0,0,0))yes;
+    else no;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
