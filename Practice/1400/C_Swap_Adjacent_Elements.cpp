7d00f711f1190a667e3393312d3f4fae8dd78a8c
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-09-27 16:38:48 +0530

    27-9-2024

---
+++ Practice/1400/C_Swap_Adjacent_Elements.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+
+int add(int a,int b,int mod){
+   int ans=(a%mod + b%mod)%mod;
+   if(ans<0){
+      ans+=mod;
+   }
+   return ans;
+}
+
+int sub(int a,int b,int mod){
+   int ans=(a%mod - b%mod)%mod;
+   if(ans<0)ans+=mod;
+   return ans;
+}
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n),pos(n+1),pre(n);
+    input(v);
+    string s;cin>>s;
+    for(int a=0;a<n;a++){
+        pos[v[a]]=a;
+    }
+    pre[0]=s[0]-'0';
+    for(int a=1;a<n-1;a++){
+        pre[a]=pre[a-1]+(s[a]-'0');
+    }
+    pre[n-1]=pre[n-2];
+    for(int a=1;a<n;a++){
+        if(pos[a+1]<pos[a]){
+            int r=pos[a];
+            int l=pos[a+1];
+            int sum=pre[r-1]-(l-1>=0 ? pre[l-1]:0);
+            if(sum<r-l){
+                no;
+                return;
+            }
+        }
+    }
+    yes;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
