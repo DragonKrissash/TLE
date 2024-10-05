340d620470727316b94ef41233baa09f1beef0fb
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-10-05 08:46:33 +0530

    5-10-2024

---
+++ Practice/1300/B_Turtle_and_an_Infinite_Sequence.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,m;cin>>n>>m;
+    int l=max(0LL,n-m);
+    int r=n+m;
+    int ans=0;
+    int val=1;
+    for(int a=0;a<32;a++){
+        if(r-l>val)ans+=val;
+        else if(l&(1<<a))ans+=val;
+        else if(r&(1<<a))ans+=val;
+        val*=2;
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
