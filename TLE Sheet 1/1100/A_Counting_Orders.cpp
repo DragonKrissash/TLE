cdb7b6a239aeaddb952edc8789bb7e296dcba573
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-10-06 11:21:33 +0530

    6-10-2024

---
+++ TLE Sheet 1/1100/A_Counting_Orders.cpp
@@ -0,0 +1,94 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+const int MOD=1e9+7;
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v1(n),v2(n);
+    input(v1);input(v2);
+    sort(all(v1));
+    sort(all(v2),greater<>());
+    int ans=1;
+    for(int a=0;a<n;a++){
+        int ind=n-(upper_bound(all(v1),v2[a])-v1.begin());
+        // cout<<ind<<sp;
+        // if(v2[ind-1]==v1[a]){
+        //     cout<<0<<nl;
+        //     return;
+        // }
+        if(ind-a<0){
+            ans=0;
+        }
+        else ans=ans*(ind-a)%MOD;
+    }
+    cout<<ans<<nl;
+    
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
+
+
+//template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+//template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+//
+//struct starter{
+//   high_resolution_clock::time_point t1, t2;
+//
+//   starter() {
+//       ios::sync_with_stdio(0); cin.tie(0);
+//       t1 = high_resolution_clock::now();
+//   }
+//   ~starter() {
+//      t2 = high_resolution_clock::now();
+//       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+//       cerr <<  time_span.count();
+//       }
+//} starter_;
