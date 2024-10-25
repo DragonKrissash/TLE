c4b5de3e5bb7269b43ef39794fa70a49802e0cfd
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-10-25 15:25:16 +0530

    25-10-2024

---
+++ Practice/1100/B_Bessie_and_MEX.cpp
@@ -0,0 +1,66 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n);
+    input(v);
+    int m1=0,m2=1;
+    vi p(n);
+    vi has(n+1);
+    for(int a=0;a<n;a++){
+        if(v[a]>0){
+            p[a]=m1;
+        }
+        else {
+            p[a]=m1-v[a];
+        }
+        has[p[a]]=1;
+        while(has[m1])m1++;
+    }
+    for(int a=0;a<n;a++)
+    cout<<p[a]<<sp;
+    cout<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
