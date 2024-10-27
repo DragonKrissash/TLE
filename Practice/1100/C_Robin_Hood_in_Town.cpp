0ab7d579eb8beb25b23e6b391ccc6275de6e5cb1
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-10-27 18:37:54 +0530

    27-10-2024

---
+++ Practice/1100/C_Robin_Hood_in_Town.cpp
@@ -0,0 +1,60 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n);
+    input(v);
+    if(n==1 || n==2){
+        cout<<-1<<nl;
+        return;
+    }
+    sort(all(v));
+    int sum=0;
+    for(int a=0;a<n;a++)sum+=v[a];
+    int mid=v[n/2];
+    int x=2*n*mid-sum;
+    cout<<max(x+1,0LL)<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
