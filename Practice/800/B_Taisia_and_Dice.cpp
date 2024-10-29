31ff7548da0753f48f8709d3851cb9d4863f27dc
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-10-29 15:53:09 +0530

    29-10-2024

---
+++ Practice/800/B_Taisia_and_Dice.cpp
@@ -0,0 +1,58 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,s,r;cin>>n>>s>>r;
+    int maxi=s-r;
+    cout<<maxi<<sp;
+    int x=r/(n-1);
+    int rem=r%(n-1);
+    for(int a=0;a<n-1;a++){
+        if(rem>0)cout<<x+1<<sp;
+        else cout<<x<<sp;
+        rem--;
+    }
+    cout<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
