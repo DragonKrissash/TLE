20d029eed7c1088c490af4868565157cb7905d64
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-11-03 20:56:57 +0530

    3-11-2024

---
+++ TLE Sheet 1/1200/D_Friends_and_the_Restaurant.cpp
@@ -0,0 +1,68 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi ai(n),bi(n);
+    input(ai);
+    input(bi);
+    vi v(n);
+    for(int a=0;a<n;a++){
+        v[a]=bi[a]-ai[a];
+    }
+    sort(all(v));
+    int cnt=0;
+    int i=0,j=n-1;
+    while(i<=j){
+        if(i!=j and v[i]+v[j]>=0){
+            cnt++;
+            i++;
+            j--;
+        }
+        else i++;
+    }
+    cout<<cnt<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
