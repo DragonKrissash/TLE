20a206ed1730a280e39f7290f8ce56a926aeda62
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-11-03 13:34:18 +0530

    3-11-2024

---
+++ TLE Sheet 1/1200/E_Scuza.cpp
@@ -0,0 +1,72 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,k;cin>>n>>k;
+    vi v(n);
+    input(v);
+    vi pre(n);
+    pre[0]=v[0];
+    vi maxi(n);
+    maxi[0]=v[0];
+    int mx=v[0];
+    for(int a=1;a<n;a++){
+        pre[a]=pre[a-1]+v[a];
+        mx=max(mx,v[a]);
+        maxi[a]=mx;
+    }
+    while(k--){
+        int x;cin>>x;
+        int ind=upper_bound(all(maxi),x)-maxi.begin();
+        ind--;
+        if(ind<0){
+            cout<<0<<sp;
+        }
+        else
+        cout<<max(pre[ind],0LL)<<sp;
+    }
+    cout<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
