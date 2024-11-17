4503db90ba3264eb2a88acd427304b0eeaca2eb2
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-11-17 16:28:33 +0530

    17-11-2024

---
+++ Practice/800/B_Ordinary_Numbers.cpp
@@ -0,0 +1,66 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    int len=to_string(n).size();
+    int t=n;
+    int ld=1;
+    int cnt=0;
+    while(len>0){
+        for(int a=1;a<=9;a++){
+            int num=ld*a;
+            if(num<=n)cnt++;
+            else{
+                cout<<cnt<<nl;
+                return;
+            }
+        }
+        ld=ld*10+1;
+        len--;
+    }
+    cout<<cnt<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
