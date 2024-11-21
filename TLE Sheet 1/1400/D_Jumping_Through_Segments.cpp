060ed73fb3f9774dd3b96999fdb5f6d0ef9fa00f
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-11-21 15:08:51 +0530

    21-11-2024

---
+++ TLE Sheet 1/1400/D_Jumping_Through_Segments.cpp
@@ -0,0 +1,79 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+bool f(int mid,vector<pii>&vp){
+    int n=vp.size();
+    int l=0,r=0;
+    for(int a=0;a<n;a++){
+        if(r+mid<vp[a].X or l-mid>vp[a].Y){
+            return false;
+        }
+        else {
+            l=max(l-mid,vp[a].X);
+            r=min(r+mid,vp[a].Y);
+        }
+    }
+    return true;
+}
+
+void solve(){
+    int n;cin>>n;
+    vector<pii>vp(n);
+    for(auto &i:vp){
+        cin>>i.X>>i.Y;
+    }
+    int l=0,r=1e9;
+    int ans=0;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(f(mid,vp)){
+            ans=mid;
+            r=mid-1;
+        }
+        else l=mid+1;
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
