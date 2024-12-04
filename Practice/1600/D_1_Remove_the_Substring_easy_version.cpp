971f61af8a798682131ab453624fdbe445614158
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-04 11:07:54 +0530

    4-12-2024

---
+++ Practice/1600/D_1_Remove_the_Substring_easy_version.cpp
@@ -0,0 +1,64 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    string s,t;
+    cin>>s>>t;
+    int ans=0;
+    int n=s.size(),m=t.size();
+    for(int a=0;a<n;a++){
+        for(int b=a;b<n;b++){
+            int pos=0;
+            for(int p=0;p<n;p++){
+                if(a<=p and p<=b)continue;
+                if(pos<m and s[p]==t[pos])pos++;
+            }
+            if(pos==m)
+            ans=max(ans,b-a+1);
+        }
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
