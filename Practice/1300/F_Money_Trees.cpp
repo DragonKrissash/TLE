36be50219a33b4334e40b3fe45d50e3347f6c0ea
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-06 16:50:56 +0530

    6-12-2024

---
+++ Practice/1300/F_Money_Trees.cpp
@@ -0,0 +1,67 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,k;cin>>n>>k;
+    vi ai(n),hi(n);
+    input(ai);
+    input(hi);
+    int sum=0,ans=0;
+    for(int i=0,j=0;j<n;){
+        sum+=ai[j];
+        while(sum>k){
+            sum-=ai[i];
+            i++;
+        }
+        ans=max(ans,j-i+1);
+        if(j+1<n and hi[j]%hi[j+1] !=0){
+            i=j+1;
+            sum=0;
+        }
+        j++;
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
