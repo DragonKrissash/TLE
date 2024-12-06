0427bf9ac89775ae636b580cc5a57ef6dec0e1fa
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-06 13:46:30 +0530

    6-12-2024

---
+++ Edu/Two Pointer/E_Knapsack_on_a_Segment.cpp
@@ -0,0 +1,65 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,w;cin>>n>>w;
+    vi wei(n),co(n);
+    input(wei);
+    input(co);
+    int sum=0,cost=0,ans=0;
+    for(int l=0,r=0;r<n;){
+        sum+=wei[r];
+        cost+=co[r];
+        while(sum>w){
+            sum-=wei[l];
+            cost-=co[l];
+            l++;
+        }
+        ans=max(ans,cost);
+        r++;
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
