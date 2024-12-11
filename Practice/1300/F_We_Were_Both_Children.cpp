cad16a2ba4d1a2f7ea85e8cc5ebb4989450c7fca
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-11 10:14:27 +0530

    11-12-2024

---
+++ Practice/1300/F_We_Were_Both_Children.cpp
@@ -0,0 +1,69 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n);
+    input(v);
+    vi freq(200001);
+    for(int a=0;a<n;a++){
+        if(v[a]>2e5)continue;
+        freq[v[a]]++;
+    }
+    vi arr(n+1);
+    for(int a=1;a<=n;a++){
+        if(freq[a]>=1){
+            for(int b=a;b<=n;b+=a){
+                arr[b]+=freq[a];
+            }
+        }
+    }
+    int ans=0;
+    for(int a:arr){
+        ans=max(ans,a);
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
