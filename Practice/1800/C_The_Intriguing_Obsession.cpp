94b6bd9bb851c31a58d9bc888177b064066a7328
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-12 17:30:55 +0530

    12-12-2024

---
+++ Practice/1800/C_The_Intriguing_Obsession.cpp
@@ -0,0 +1,95 @@
+
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+const int MOD=998244353;
+int mod(int a, int m = MOD) {
+    return a % m;
+}
+template <class T> class Math {
+  public:
+    vector<T> fact, invfact;
+    Math() {}
+    Math(int n) {
+        fact.resize(n);
+        invfact.resize(n);
+        fact[0] = invfact[0] = 1;
+        for (int i = 1; i < n; i++) {
+            fact[i] = mod(i * fact[i - 1]);
+            invfact[i] = modinv(fact[i]);
+        }
+    }
+    T modinv(T x, T m = MOD) { return expo(x, m - 2, m); }
+    T expo(T base, T exp, T m = MOD) {
+        T res = 1;
+        while (exp) {
+            if (exp & 1)
+                res = mod(res * base, m);
+            base = mod(base * base, m);
+            exp >>= 1;
+        }
+        return res;
+    }
+    T choose(T n, T k) {
+        if (k < 0 || k > n)
+            return 0;
+        T ans = fact[n];
+        ans = mod(ans * invfact[n - k]);
+        ans = mod(ans * invfact[k]);
+        return ans;
+    }
+};
+int sol(int a,int b){
+    Math<int> m(max(a,b)+1);
+    int res=0;
+    for(int i=0;i<=min(a,b);i++){
+        res=(res+(((m.choose(a,i))*m.choose(b,i))%MOD * m.fact[i])%MOD)%MOD;
+    }
+    return res;
+}
+
+void solve(){
+    int a,b,c;cin>>a>>b>>c;
+    cout<<((sol(a,b)*sol(b,c))%MOD*sol(c,a))%MOD<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
