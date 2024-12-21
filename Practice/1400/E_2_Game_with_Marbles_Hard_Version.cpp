3f83baafdf4d93ed61d02eb241c1b5e941db058a
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-21 14:11:49 +0530

    21-12-2024

---
+++ Practice/1400/E_2_Game_with_Marbles_Hard_Version.cpp
@@ -0,0 +1,69 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vector<pii>vp(n);
+    for(int a=0;a<n;a++){
+        cin>>vp[a].X;
+    }
+    for(auto &a:vp){
+        cin>>a.Y;
+    }
+    sort(all(vp),[&](pii &i,pii &j){
+        return i.X+i.Y>j.X+j.Y;
+    });
+    int ans=0;
+    for(int a=0;a<n;a++){
+        if(a&1){
+            ans-=vp[a].Y-1;
+        }
+        else ans+=vp[a].X-1;
+    }
+    cout<<ans<<nl;
+
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
