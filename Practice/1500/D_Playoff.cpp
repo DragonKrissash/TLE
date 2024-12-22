0f8335ec804fad7ce276a4070248f2e31f4e8631
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-12-22 11:23:14 +0530

    22-12-2024

---
+++ Practice/1500/D_Playoff.cpp
@@ -0,0 +1,63 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    string s;cin>>s;
+    int cn1=0,cn0=0;
+    for(auto c:s){
+        if(c=='1')cn1++;
+        else cn0++;
+    }
+    int l=(1LL<<cn1),r=(1LL<<n)-(1LL<<cn0)+1;
+    int num=1LL<<n;
+    for(int a=l;a<=r;a++){
+        cout<<a<<sp;
+    }
+    // cout<<nl;
+    // cout<<l<<sp<<r<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
