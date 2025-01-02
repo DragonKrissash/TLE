eb3983403ed50e7050ec3ba65e6e182646cf1c9c
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-01-02 20:59:05 +0530

    2-1-2025

---
+++ Practice/1000/A_Chat_room.cpp
@@ -0,0 +1,66 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    string s;cin>>s;
+    string t="hello";
+    int i=0,j=0;
+    int n=s.size();
+    while(j<n){
+        if(s[j]==t[i]){
+            i++;
+        }
+        if(i==t.size()){
+            yes;
+            return;
+        }
+        j++;
+    }
+    if(i==t.size())
+    yes;
+    else no;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
