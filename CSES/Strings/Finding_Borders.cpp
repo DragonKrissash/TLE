192ba553afaeac7e36ac265946843d68533a2faa
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-01-29 19:46:44 +0530

    29-1-2025

--- CSES/Strings/Finding_Borders.cpp
+++ CSES/Strings/Finding_Borders.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #define int long long
 #define vi vector<int>
 #define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
 #define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
 #define X first
 #define Y second
@@ -18,6 +19,8 @@ using namespace std;
 #define mx(v) max_element(v.begin(),v.end())
 #define mn(v) min_element(v.begin(),v.end())
 #define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
 const int MOD = 1e9+7;
 
 #define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
@@ -35,47 +38,51 @@ signed main(){
     }
 }
 
-int bin_power_mod(int base,int exp,int mod){
-    base%=mod;
-    int res=1;
-    while(exp>0){
-        if(exp&1){
-            res=res * base % mod;
+struct HASH{
+    vector<pii>pow,has;
+    const int mod=1e9+7,b1=5689,b2=8861;
+    HASH(string s){
+        int n=s.size();
+        pow.assign(n+1,{1,1});
+        has.assign(n+1,{0,0});
+        for(int a=0;a<n;a++){
+            has[a+1]={
+                (has[a].X*b1+(s[a]-'a'+1))%mod,
+                (has[a].Y*b2+(s[a]-'a'+1))%mod
+            };
+            pow[a+1]={
+                (pow[a].X*b1)%mod,
+                (pow[a].Y*b2)%mod
+            };
         }
-        base=base * base %mod;
-        exp>>=1;
     }
-    return res;
-}
+    pii get(int l,int r){
+        l++;r++;
+        int h1=(has[r].X-(has[l-1].X*pow[r-l+1].X)%mod+mod)%mod;
+        int h2=(has[r].Y-(has[l-1].Y*pow[r-l+1].Y)%mod+mod)%mod;
+        return {h1,h2};
+    }
 
-int inverse_mod(int base,int mod){
-    return bin_power_mod(base,mod-2,mod);
-}
+};
 
 void solve(){
     string s;cin>>s;
     int n=s.size();
-    vector<int>has1(n,0),has2(n,0);
-    int p1=31,mod1=1e9+7;
-    int p2=37,mod2=1e9+9;
-    has1[0]=s[0]-'a'+1;
-    has2[0]=s[0]-'a'+1;
-    for(int a=1;a<n;a++){
-        has1[a]=(has1[a-1]+(s[a]-'a'+1)*bin_power_mod(p1,a,mod1))%mod1;
-        // has2[a]=(has2[a-1]+(s[a]-'a'+1)*bin_power_mod(p2,a,mod2))%mod2;
+    HASH h(s);
+    vi ans;
+    for(int a=0;a<n-1;a++){
+        pii h1=h.get(0,a);
+        pii h2=h.get(n-1-a,n-1);
+        if(h1==h2){
+            ans.pb(a+1);
+        }
     }
-    for(int a=1;a<n;a++){
-        int pre1=has1[a-1];
-        int suf1=((has1[n-1]-has1[n-1-a] + mod1)*inverse_mod(bin_power_mod(p1,n-a,mod1),mod1))%mod1;
-        // int pre2=has2[a-1];
-        // int suf2=((has2[n-1]-has2[n-1-a] + mod2)*inverse_mod(bin_power_mod(p2,n-a,mod2),mod2))%mod2;
-        if(pre1==suf1)cout<<a<<sp;
-    }
+    for(int a:ans)cout<<a<<sp;
 }
 
 
 
- // Question od be ded
+// Question od be ded
 // constraints dekh be ded
 // shortcut aas satni time waste karnako be ded
 // tujere nammakam thonge, solve karsaktis
