6f065953a2a1136dd194c43a722887c036bb1a2c
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-02-03 11:08:24 +0530

    3-2-2025

---
+++ Contests/A_Milya_and_Two_Arrays.cpp
@@ -0,0 +1,64 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    set<int>st1,st2;
+    for(int a=0;a<n;a++){
+        int x;cin>>x;
+        st1.insert(x);
+    }
+    for(int a=0;a<n;a++){
+        int x;cin>>x;
+        st2.insert(x);
+    }
+    int s1=st1.size(),s2=st2.size();
+    // cout<<s1<<sp<<s2;
+    if(s1==1 and s2==1)no;
+    else if((s1==1 and s2==2) || (s2==1 and s1==2))no;
+    else yes;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
