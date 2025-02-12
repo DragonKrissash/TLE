ac7dc472beb7e02717b1c5b9632df6fceee5aaac
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-02-12 20:24:50 +0530

    12-2-2025

---
+++ Contests/B_Two_Large_Bags.cpp
@@ -0,0 +1,70 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"Yes"<<nl
+#define no cout<<"No"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n);
+    input(v);
+    map<int,int>mp;
+    set<int>st;
+    for(int a=0;a<n;a++){
+        mp[v[a]]++;
+        st.insert(v[a]);
+    }
+    while(!st.empty()){
+        int num=*st.begin();
+        if(mp[num]==1){
+            no;
+            return;
+        }
+        mp[num+1]+=mp[num]-2;
+        st.erase(num);
+        if(mp[num+1])
+        st.insert(num+1);
+    }
+    yes;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
