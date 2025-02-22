276fb1f0a0fbdbb60866a364117890ac9049074e
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-02-22 23:19:48 +0530

    22-2-2025

---
+++ Practice/1200/D_Counting_Pairs.cpp
@@ -0,0 +1,65 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    vi v(n);
+    input(v);
+    sort(all(v));
+    int cnt=0;
+    int sum=sm(v);
+    for(int a=0;a<n;a++){
+        int xsum=sum-(x+v[a]);
+        int ysum=sum-(y+v[a]);
+        int yind=lower_bound(v.begin()+a+1,v.end(),ysum)-v.begin();
+        int xind=upper_bound(v.begin()+a+1,v.end(),xsum)-v.begin();
+        xind--;
+        if(xind>=yind)cnt+=xind-yind+1;
+    }
+    cout<<cnt<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
