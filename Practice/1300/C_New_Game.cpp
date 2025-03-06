90eb84ae0e2722de716f5d516741c390c0696c63
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-03-06 21:46:59 +0530

    6-3-2025

---
+++ Practice/1300/C_New_Game.cpp
@@ -0,0 +1,63 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,k;cin>>n>>k;
+    vi v(n);
+    input(v);
+    sort(all(v));
+    int ans=0;
+    int i=0,j=0;
+    for(;i<n;i++){
+        j=max(i,j);
+        while(j+1<n and v[j+1]-v[j]<=1 and v[j+1]-v[i]<k){
+            j++;
+        }
+        ans=max(ans,j-i+1);
+    }
+    cout<<ans<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
