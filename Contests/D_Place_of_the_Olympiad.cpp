1ee2fd9d3c277d9506306065bc4dda51940b1e79
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-04-10 14:11:25 +0530

    10-4-2025

---
+++ Contests/D_Place_of_the_Olympiad.cpp
@@ -0,0 +1,64 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n,m,x;cin>>n>>m>>x;
+    if(x<=n or 2*x-1<=m){
+        cout<<1<<nl;
+        return;
+    }
+    else{
+        int num=x/n;
+        if(x%n)num++;
+        int req=num-1;
+        int num2=m-num;
+        if(num2==0)cout<<num<<nl;
+        else if(num2>=req)cout<<1<<nl;
+        else
+        cout<<m/(num2+1)<<nl;
+    }
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
