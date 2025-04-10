a09f7549b8ec6d713e7aecb66a779c19855e3934
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-04-10 14:10:36 +0530

    10-4-2025

---
+++ Contests/A_Olympiad_Date.cpp
@@ -0,0 +1,72 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    multiset<int>st;
+    int n;
+    cin>>n;
+    vi v(n);
+    input(v);
+    st.insert(0);
+    st.insert(1);
+    st.insert(0);
+    st.insert(3);
+    st.insert(2);
+    st.insert(0);
+    st.insert(2);
+    st.insert(5);
+    for(int a=0;a<n;a++){
+        if(st.count(v[a])){
+            st.erase(st.find(v[a]));
+        }
+        if(st.empty()){
+            cout<<a+1<<nl;
+            return;
+        }
+    }
+    cout<<0<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
