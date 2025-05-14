51ae645d0979e5cd62665b5fbb24b3e4e6815547
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-05-14 18:14:49 +0530

    14-5-2025

---
+++ CSES/DP/Dice_Combinations.cpp
@@ -0,0 +1,64 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    // cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+int add(int a,int b){
+    return ((a%MOD)+(b%MOD))%MOD;
+}
+
+void solve(){
+    int n;cin>>n;
+    vi dp(max(7LL,n+1));
+    for(int a=1;a<=6;a++)dp[a]=1;
+    for(int a=1;a<=n;a++){
+        int sum=0;
+        for(int b=1;b<=6;b++){
+            if(a-b>=0)sum=add(sum,dp[a-b]);
+        }
+        dp[a]=add(sum,dp[a]);
+    }
+    cout<<dp[n]<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
