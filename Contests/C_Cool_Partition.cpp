897aa8254548cedfc071dcffa6962656f49a8076
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2025-06-09 21:34:30 +0530

    9-6-2025

---
+++ Contests/C_Cool_Partition.cpp
@@ -0,0 +1,65 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define int long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define input(v) for(int a=0;a<v.size();a++)cin>>v[a]
+#define X first
+#define Y second
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define sp " "
+#define repi(a,b,c) for(int i=a;i<=b;i+=c)
+#define pii pair<int,int>
+#define mx(v) max_element(v.begin(),v.end())
+#define mn(v) min_element(v.begin(),v.end())
+#define sm(v) accumulate(v.begin(),v.end(),0LL)
+#define pb push_back
+#define ld long double
+const int MOD = 1e9+7;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+void solve();
+
+signed main(){
+
+    fastio
+    int tc;
+    tc=1;
+    cin>>tc;
+    while(tc--){
+    solve();
+    }
+}
+
+void solve(){
+    int n;cin>>n;
+    vi v(n);
+    input(v);
+    set<int>s1,s2;
+    s1.insert(v[0]);
+    int cnt=0;
+    for(int a=0;a<n;a++){
+        s2.insert(v[a]);
+        if(s1.count(v[a]))s1.erase(s1.find(v[a]));
+        if(s1.empty()){
+            cnt++;
+            s1=s2;
+            s2.clear();
+        }
+    }
+    cout<<cnt<<nl;
+}
+
+
+
+// Question od be ded
+// constraints dekh be ded
+// shortcut aas satni time waste karnako be ded
+// tujere nammakam thonge, solve karsaktis
